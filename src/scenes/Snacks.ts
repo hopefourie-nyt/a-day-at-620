d9d92a574f7147d49b4ad03d8e9b84ffad8bd382
Hope Fourie <hope.fourie@nytimes.com>
2023-07-21 10:14:04 -0400

    fix

--- src/scenes/Snacks.ts
+++ src/scenes/Snacks.ts
@@ -19,52 +19,57 @@ export default class Snacks extends Phaser.Scene {
     const y = 430;
 
     // Background
-    this.physics.world.setBounds(0,0,5000, 5000)
-    this.add.rectangle(600, 2500, 5000, 5500, 0xE6FFFC)
-    this.add.rectangle(350, 5050, 5000, 200, 0xC8C8C8)
+    this.physics.world.setBounds(0, 0, 5000, 5000);
+    this.add.rectangle(600, 2500, 5000, 5500, 0xe6fffc);
+    this.add.rectangle(350, 5050, 5000, 200, 0xc8c8c8);
     // Top Floor
-    let topFloor = this.add.rectangle(350, 3200, 5000, 200, 0xC8C8C8)
+    let topFloor = this.add.rectangle(350, 3200, 5000, 200, 0xc8c8c8);
     this.physics.world.enableBody(topFloor);
     topFloor.body = <Phaser.Physics.Arcade.Body>topFloor.body;
     topFloor.body.setImmovable(true);
-    let snack = this.add.image(2267, 3230, "snack").setOrigin(.5)
+    let snack = this.add.image(2267, 3230, "snack").setOrigin(0.5);
     this.physics.world.enableBody(snack);
     snack.body = <Phaser.Physics.Arcade.Body>snack.body;
     snack.body.setImmovable(true);
     // Corridor
     const corridorGroup = this.physics.add.staticGroup();
-    for (let i = 0; i<100; i++){
-      let newX = 220 + (i*18)
-      let newY = 4990 - (i*17)
-      let corridor =corridorGroup.create(newX, newY, "stair");
-      corridor.flipX= true
+    for (let i = 0; i < 100; i++) {
+      let newX;
+      if (this.playerImageKey === "julian") {
+        newX = 220 + i * 18;
+      } else {
+        newX = 210 + i * 18;
+      }
+      let newY = 4990 - i * 17;
+      let corridor = corridorGroup.create(newX, newY, "stair");
+      corridor.flipX = true;
     }
 
     // Corridor 1
     const corridorGroup1 = this.physics.add.staticGroup();
-    for (let i = 0; i<100; i++){
-      let newX = 730 + (i*18)
-      let newY = 4990 - (i*17)
-      let corridor =corridorGroup1.create(newX, newY, "stair");
-      corridor.flipX= true
+    for (let i = 0; i < 100; i++) {
+      let newX = 730 + i * 18;
+      let newY = 4990 - i * 17;
+      let corridor = corridorGroup1.create(newX, newY, "stair");
+      corridor.flipX = true;
     }
 
     // Corridor 2
     const corridorGroup2 = this.physics.add.staticGroup();
-    for (let i = 0; i<100; i++){
-      let newX = -30 + (i*18)
-      let newY = 4990 - (i*17)
-      let corridor =corridorGroup2.create(newX, newY, "stair");
-      corridor.flipX= true
+    for (let i = 0; i < 100; i++) {
+      let newX = -30 + i * 18;
+      let newY = 4990 - i * 17;
+      let corridor = corridorGroup2.create(newX, newY, "stair");
+      corridor.flipX = true;
     }
 
     // Stairs
     const stairGroup = this.physics.add.staticGroup();
-    for (let i = 0; i<100; i++){
-      let newX = 470 + (i*18)
-      let newY = 4990 - (i*17)
-      let stair =stairGroup.create(newX, newY, "stair");
-      stair.flipX= true
+    for (let i = 0; i < 100; i++) {
+      let newX = 470 + i * 18;
+      let newY = 4990 - i * 17;
+      let stair = stairGroup.create(newX, newY, "stair");
+      stair.flipX = true;
     }
 
     this.player = new Snacker(this, 310, 4990, this.playerImageKey);
@@ -72,7 +77,10 @@ export default class Snacks extends Phaser.Scene {
     this.physics.add.collider(this.player, corridorGroup);
 
     // Eric
-    let eric = this.add.sprite(140, 4950, "eric").setOrigin(0.5).setScale(3/4);
+    let eric = this.add
+      .sprite(140, 4950, "eric")
+      .setOrigin(0.5)
+      .setScale(3 / 4);
     eric.body = <Phaser.Physics.Arcade.Body>eric.body;
 
     this.physics.world.enableBody(eric);
@@ -80,24 +88,29 @@ export default class Snacks extends Phaser.Scene {
     eric.body.setVelocity(47.25, -41.6);
 
     // Sam
-    let sam = this.add.sprite(640, 4950, "sam").setOrigin(0.5).setScale(3/4);
+    let sam = this.add
+      .sprite(640, 4950, "sam")
+      .setOrigin(0.5)
+      .setScale(3 / 4);
     sam.body = <Phaser.Physics.Arcade.Body>sam.body;
 
     this.physics.world.enableBody(sam);
 
     sam.body.setVelocity(39.4, -34.7);
 
-    
     let camera = this.cameras.main;
     camera.startFollow(this.player);
-    camera.setFollowOffset(0,100);
+    camera.setFollowOffset(0, 100);
 
     this.physics.add.overlap(this.player, snack, () => {
       this.time.addEvent({
         delay: 1000,
         callback: () => {
-          this.scene.launch("LongDay", {playerImageKey: this.playerImageKey, cupcake: true});
-        this.scene.pause()
+          this.scene.launch("LongDay", {
+            playerImageKey: this.playerImageKey,
+            cupcake: true,
+          });
+          this.scene.pause();
         },
         loop: false,
       });
@@ -107,8 +120,11 @@ export default class Snacks extends Phaser.Scene {
       this.time.addEvent({
         delay: 1000,
         callback: () => {
-          this.scene.launch("LongDay", {playerImageKey: this.playerImageKey, cupcake: false});
-        this.scene.pause()
+          this.scene.launch("LongDay", {
+            playerImageKey: this.playerImageKey,
+            cupcake: false,
+          });
+          this.scene.pause();
         },
         loop: false,
       });
@@ -118,17 +134,26 @@ export default class Snacks extends Phaser.Scene {
       this.time.addEvent({
         delay: 1000,
         callback: () => {
-          this.scene.launch("LongDay", {playerImageKey: this.playerImageKey, cupcake: false});
-        this.scene.pause()
+          this.scene.launch("LongDay", {
+            playerImageKey: this.playerImageKey,
+            cupcake: false,
+          });
+          this.scene.pause();
         },
         loop: false,
       });
     });
 
-    this.add.rectangle(310, 5070, 620, 70, 0x5F9A93).setOrigin(.5)
-    this.add.rectangle(310, 5070, 600, 50, 0x84B7B1).setOrigin(.5)
-    this.add.text(310, 5070, "Alternate the up and right arrow keys to race up the stairs", {color: '#000000'}).setOrigin(.5)
-
+    this.add.rectangle(310, 5070, 620, 70, 0x5f9a93).setOrigin(0.5);
+    this.add.rectangle(310, 5070, 600, 50, 0x84b7b1).setOrigin(0.5);
+    this.add
+      .text(
+        310,
+        5070,
+        "Alternate the up and right arrow keys to race up the stairs",
+        { color: "#000000" }
+      )
+      .setOrigin(0.5);
   }
   update(): void {
     this.player?.update();
