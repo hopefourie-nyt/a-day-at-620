5eded7e50e9959f014b2bde2b54d2062a64c549d
Julian Meltzer <julian.meltzer@nytimes.com>
2023-07-24 12:25:46 -0400

    move to public folder

--- src/scenes/PreLoader.ts
+++ src/scenes/PreLoader.ts
@@ -22,66 +22,66 @@ export default class PreLoader extends Phaser.Scene {
     );
     // Music
     this.load.audio('lofi',
-    "src/assets/music/lofi.mp3")
+    "music/lofi.mp3")
 
     // Backgrounds
     this.load.image(
       "conference-room",
-      "src/assets/backgrounds/conference-room.png"
+      "backgrounds/conference-room.png"
     );
-    this.load.image("nytb", "src/assets/backgrounds/nytb.png");
-    this.load.image("elevators_1", "src/assets/backgrounds/elevators.png");
-    this.load.image("subway", "src/assets/backgrounds/subway.png");
-    this.load.image("city-1", "src/assets/backgrounds/city-1.png");
-    this.load.image("city-2", "src/assets/backgrounds/city-2.png");
-    this.load.image("sunset", "src/assets/backgrounds/sunset.png");
+    this.load.image("nytb", "backgrounds/nytb.png");
+    this.load.image("elevators_1", "backgrounds/elevators.png");
+    this.load.image("subway", "backgrounds/subway.png");
+    this.load.image("city-1", "backgrounds/city-1.png");
+    this.load.image("city-2", "backgrounds/city-2.png");
+    this.load.image("sunset", "backgrounds/sunset.png");
 
     // Tilemaps
     this.load.tilemapTiledJSON(
       "elevators_map",
-      "src/assets/tilemaps/elevator.json"
+      "tilemaps/elevator.json"
     );
     this.load.tilemapTiledJSON(
       "room_map",
-      "src/assets/tilemaps/conference-room.json"
+      "tilemaps/conference-room.json"
     );
 
     // Overlays
-    this.load.image("panel", "src/assets/backgrounds/panel.png");
-    this.load.image("laptop", "src/assets/backgrounds/laptop.png");
+    this.load.image("panel", "backgrounds/panel.png");
+    this.load.image("laptop", "backgrounds/laptop.png");
 
     // Sprites
-    this.load.image("corridor", "src/assets/sprites/corridor.png");
-    this.load.image("snack", "src/assets/sprites/snack.png");
+    this.load.image("corridor", "sprites/corridor.png");
+    this.load.image("snack", "sprites/snack.png");
 
-    this.load.image("stairs", "src/assets/sprites/stairs.png");
-    this.load.image("union-bug", "src/assets/sprites/union-bug.png");
-    this.load.image("eric", "src/assets/sprites/eric.png");
-    this.load.image("sam", "src/assets/sprites/sam.png");
-    this.load.image("ken", "src/assets/sprites/ken.png");
-    this.load.image("tristan", "src/assets/sprites/tristan.png");
-    this.load.image("stair", "src/assets/sprites/stair.png");
+    this.load.image("stairs", "sprites/stairs.png");
+    this.load.image("union-bug", "sprites/union-bug.png");
+    this.load.image("eric", "sprites/eric.png");
+    this.load.image("sam", "sprites/sam.png");
+    this.load.image("ken", "sprites/ken.png");
+    this.load.image("tristan", "sprites/tristan.png");
+    this.load.image("stair", "sprites/stair.png");
 
-    this.load.image("taxi", "src/assets/sprites/taxi.png");
-    this.load.image("elevator-door", "src/assets/sprites/elevator-door.png");
-    this.load.image("logo", "src/assets/sprites/logo.png");
-    this.load.image("textbox", "src/assets/sprites/textbox.png");
-    this.load.image("desk", "src/assets/sprites/desk.png");
-    this.load.image("chair", "src/assets/sprites/chair.png");
-    this.load.image("big-fist", "src/assets/sprites/big-fist.png");
+    this.load.image("taxi", "sprites/taxi.png");
+    this.load.image("elevator-door", "sprites/elevator-door.png");
+    this.load.image("logo", "sprites/logo.png");
+    this.load.image("textbox", "sprites/textbox.png");
+    this.load.image("desk", "sprites/desk.png");
+    this.load.image("chair", "sprites/chair.png");
+    this.load.image("big-fist", "sprites/big-fist.png");
 
     // Characters
-    this.load.image("julian", "src/assets/sprites/julian.png");
-    this.load.image("hope", "src/assets/sprites/hope.png");
-    this.load.image("variant", "src/assets/sprites/variant.png");
-    this.load.image('conference-room', 'src/assets/backgrounds/conference-room.png')
+    this.load.image("julian", "sprites/julian.png");
+    this.load.image("hope", "sprites/hope.png");
+    this.load.image("variant", "sprites/variant.png");
+    this.load.image('conference-room', 'backgrounds/conference-room.png')
 		this.load.tilemapTiledJSON(
 			"room_map",
-			"src/assets/tilemaps/conference-room.json"
+			"tilemaps/conference-room.json"
 		  )
-    this.load.image("big-fist", 'src/assets/sprites/big-fist.png');
-    this.load.image("fly", 'src/assets/sprites/fly.png');
-    this.load.image("slack-box", 'src/assets/sprites/slack-box.png');
+    this.load.image("big-fist", 'sprites/big-fist.png');
+    this.load.image("fly", 'sprites/fly.png');
+    this.load.image("slack-box", 'sprites/slack-box.png');
 
   }
 
